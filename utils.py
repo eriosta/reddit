f8169bb37c26530bc0f39ecc56a149593fab894c
eriosta <84827743+eriosta@users.noreply.github.com>
2023-09-04 18:02:14 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,22 +1,9 @@
 def get_comments(data, subreddit, post_id):
-    """
-    This function takes a dictionary of data, a subreddit, and a post id,
-    and returns a list of comments that match the given post id in the given subreddit.
-
-    Parameters:
-    data (dict): A dictionary of data.
-    subreddit (str): A subreddit.
-    post_id (str): A post id.
-
-    Returns:
-    list: A list of comments that match the given post id in the given subreddit.
-    """
-    comments = []
+    # Get the posts from the selected subreddit
     posts = data[subreddit]
-    post = next((post for post in posts if post['id'] == post_id), None)
-    if post is not None:
-        comments_id = post['_comments_by_id']
-        for comment in post['comments']:
-            if isinstance(comment, dict) and comment['id'] in comments_id:
-                comments.append(comment['body'])
-    return comments
+
+    # Get the selected post
+    post = next(post for post in posts if post['id'] == post_id)
+
+    # Return the comments from the selected post
+    return post['comments']
